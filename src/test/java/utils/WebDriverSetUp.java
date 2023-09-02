fe98ade70ba65cfbd04492dc866054b4444e5482
chindhubabu@gmail.com <chindhubabu@gmail.com>
2023-09-01 23:35:26 -0400

    check

--- src/test/java/utils/WebDriverSetUp.java
+++ src/test/java/utils/WebDriverSetUp.java
@@ -50,15 +50,15 @@ public class WebDriverSetUp {
                 //fo.setBinary(new FirefoxBinary(new File("~/usr/tmp/firefox/firefox.exe")));
                 //fo.setBinary("~/usr/bin/firefox.exe");
                 fo.addArguments("--headless");
-
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver(fo);
                 try {
-                    System.out.println("! 1 "+Runtime.getRuntime().exec("whoami"));
+                    System.out.println(System.getProperty("user.name"));
                     System.out.println(System.getProperty("user.dir"));
-                } catch (IOException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver(fo);
+
                 break;
 
             case EDGE:
